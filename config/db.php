e591c4711467d1e1b12ddcb019dda1ce22ea4a35
Roman Netesov <rnetesov@gmail.com>
2021-05-27 13:50:33 +0500

    db config edit

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => '\yii\db\Connection',
     'dsn' => 'mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_a83850e7eff8f1e',
     'username' => 'b450b0cd06e03f',
-    'password' => 'fa67b8c0	',
+    'password' => 'fa67b8c0',
     'charset' => 'utf8'
 ];
